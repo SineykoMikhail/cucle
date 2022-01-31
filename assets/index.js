f9bfdf3adc2141d40ef82fe17ce37416b45cc52d
Sineyko Mikhail <sineykomikhail@gmail.com>
2022-01-31 20:41:24 +0200

    add new exersise

---
+++ assets/index.js
@@ -0,0 +1,22 @@
+// for(let counter = 25;counter >= 0;counter--) {
+//   console.log(counter);
+// }
+
+// let counter = 10;
+// while (counter <= 45) {
+//   console.log(counter);
+//   counter += 5;
+// }
+
+// let sum = 0;
+// for (let num = 1; num <= 100; num++) {
+//   sum += num;
+// }
+// alert(sum);
+
+while (true) {
+  let answer = prompt("Ведите ответ 2+2*2");
+  if (answer === 6) {
+    break;
+  }
+}
