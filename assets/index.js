9bb5f9448e86f3b343a142199ec90405c6974c40
Sineyko Mikhail <sineykomikhail@gmail.com>
2022-02-07 20:52:50 +0200

    add prototype and cars

--- assets/index.js
+++ assets/index.js
@@ -14,9 +14,59 @@
 // }
 // alert(sum);
 
-while (true) {
-  let answer = prompt("Ведите ответ 2+2*2");
-  if (answer === 6) {
-    break;
-  }
+// while (true) {
+//   let answer = prompt("Ведите ответ 2+2*2");
+//   if (answer === 6) {
+//     break;
+//   }
+// }
+// let nresult = "\n";
+// let number2 = 1;
+
+// for (let i = 1; i <= 10; i++) {
+//   for (let j = 1; j < 11; j++) {
+//     result += i * j + "";
+//   }
+//   console.log((result += "\n"));
+// }
+
+// const number = [1, 2, 3, 4, 5, 6, 7 ];
+// number.forEach(callback);
+// function callback(currentName) {
+//   console.log(currentName ** 2);
+// }
+
+function Cars(
+  manufacture,
+  model,
+  color,
+  releaseYer,
+  maxSpeed,
+  fuelConsumption,
+  volume
+) {
+  this.manufacture = manufacture;
+  this.model = model;
+  this.color = color;
+  this.releaseYer = releaseYer;
+  this.speed = 0;
+  this.maxSpeed = maxSpeed;
+  this.fuelConsumption = fuelConsumption;
+  this.volume = volume;
+  this.__proto__ = carPrototype;
 }
+const carPrototype = new CarPrototype();
+function CarPrototype() {
+  this.accelerate = function () {
+    return this.speed = this.speed +10;
+  };
+  this.braking = function () {
+    return (this.speed -= 10);
+  };
+  this.stoped = function () {
+    return (this.speed = 0);
+  };
+}
+
+const cars1 = new Cars("Tesla", "model X", "red", "2021", "200", "30", "60");
+
